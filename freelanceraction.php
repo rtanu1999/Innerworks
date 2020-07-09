74fb5b765c2d6722a9d88c6497c8d97ebe0b52a3
kole-swapnil <swapnilkole7500@gmail.com>
2020-07-09 13:05:40 +0530

    freelanceraction.php styling fixed

--- freelanceraction.php
+++ freelanceraction.php
@@ -7,10 +7,10 @@ include_once 'DbConnection/DbConnectionHelper.php';
 	form {
   width: 50%;
   height:50%;
-  margin: 100px auto;
+  margin: auto;
   padding: 30px;
 }
-input {
+input { 
   width: 100%;
   border: 1px solid #f1e1e1;
   display: block;
@@ -94,44 +94,201 @@ td {
       </div>
     </div>
   </nav>
+  <div class="main-content" id="panel">
+    <!-- Topnav -->
+    <nav class="navbar navbar-top navbar-expand navbar-dark bg-default border-bottom">
+      <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <!-- Search form -->
+         
+          <!-- Navbar links -->
+          <ul class="navbar-nav align-items-center  ml-md-auto ">
+            <li class="nav-item d-xl-none">
+              <!-- Sidenav toggler -->
+              <div class="pr-3 sidenav-toggler sidenav-toggler-dark" data-action="sidenav-pin" data-target="#sidenav-main">
+                <div class="sidenav-toggler-inner">
+                  <i class="sidenav-toggler-line"></i>
+                  <i class="sidenav-toggler-line"></i>
+                  <i class="sidenav-toggler-line"></i>
+                </div>
+              </div>
+            </li>
+            <li class="nav-item d-sm-none">
+              <a class="nav-link" href="#" data-action="search-show" data-target="#navbar-search-main">
+                <i class="ni ni-zoom-split-in"></i>
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="ni ni-bell-55"></i>
+              </a>
+              <div class="dropdown-menu dropdown-menu-xl  dropdown-menu-right  py-0 overflow-hidden">
+                <!-- Dropdown header -->
+                <div class="px-3 py-3">
+                  <h6 class="text-sm text-muted m-0">You have <strong class="text-primary">13</strong> notifications.</h6>
+                </div>
+                <!-- List group -->
+                <div class="list-group list-group-flush">
+                  
+          <ul class="navbar-nav align-items-center  ml-auto ml-md-0 ">
+            <li class="nav-item dropdown">
+              <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <div class="media align-items-center">
+                  <span class="avatar avatar-sm rounded-circle">
+                    <img alt="Image placeholder" src="upload/<?php echo $_SESSION['image']; ?>">
+                  </span>
+                  <div class="media-body  ml-2  d-none d-lg-block">
+                    <span class="mb-0 text-sm  font-weight-bold">xxx</span>
+                  </div>
+                </div>
+              </a>
+              <div class="dropdown-menu  dropdown-menu-right ">
+                <div class="dropdown-header noti-title">
+                  <h6 class="text-overflow m-0">Welcome!</h6>
+                </div>
+                <a href="#!" class="dropdown-item">
+                  <i class="ni ni-single-02"></i>
+                  <span>My profile</span>
+                </a>
+                
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- Header -->
+    <!-- Header -->
+    <div class="header pb-6 d-flex align-items-center" style="min-height: 500px; background-image: url(assets/img/theme/Theme.jpg); background-size: cover; background-position: center top;">
+      <!-- Mask -->
+      <span class="mask bg-gradient-default opacity-8"></span>
+      <!-- Header container -->
+      <div class="container-fluid d-flex align-items-center">
+        <div class="row">
+          <div class="col-lg-7 col-md-10">
+            <h1 class="display-2 text-white">Hello <?php echo $_SESSION['contactperson']; ?></h1>
+          </div>
+        </div>
+    </div>
+    </div>
+    <!-- Page content -->
+    <div class="container-fluid mt--6">
+      <div class="row">
+        <div class="col-xl-4 order-xl-2">
+          <div class="card card-profile">
+            <img src="assets/img/theme/img-1-1000x600.jpg" alt="Image placeholder" class="card-img-top">
+            <div class="row justify-content-center">
+              <div class="col-lg-3 order-lg-2">
+                <div class="card-profile-image">
+                  <a href="#">
+                     <img src="upload/<?php echo $_SESSION['image']; ?>" class="rounded-circle">
+                  </a>
+                </div>
+              </div>
+            </div>
+            
+            <div class="card-body pt-0">
+              <div class="row">
+                <div class="col">
+                  <div class="card-profile-stats d-flex justify-content-center">
+                   
+                  </div>
+                </div>
+              </div>
+              <div class="text-center">
+                <h5 class="h3">
+                  <span class="font-weight-light"></span>
+                </h5>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-8 order-xl-1">
+          <div class="card">
+            <div class="card-header">
+              <div class="row align-items-center">
+                <div class="col-8">
+                  <h3 class="mb-0">Freelancer Documents Upload </h3>
+                </div>
 
-    <div class="container-fluid">
-    	<h2 style="margin-left: 25%;">Freelancer Documents Upload </h2>
-    	<form action="uplod.php" method="post" enctype="multipart/form-data">
-    		<div class="row">
-    			<div class="col-md-6 ">
-    				<h2>AADHAR CARD</h2>
+              </div>
+            </div>
+            <div class="card-body">
+            <form action="uplod.php" method="post" enctype="multipart/form-data" style="width:100%">
+    		<div class="row" >
+    			<div class="col-md-4 ">
+    				<h2>AADHAR</h2>
     			</div>
-    			<div class="col-md-6 ">
+    			<div class="col-md-8">
     			  	<input type="file" name="myfile1" required="required" accept=".pdf"></br>
     			</div>
     		</div>
     		</br>
     		<div class="row">
-    			<div class="col-md-6 ">
+    			<div class="col-md-4 ">
     				<h2>PAN CARD</h2>
     			</div>
-    			<div class="col-md-6 col-offset-6">
+    			<div class="col-md-8">
     			  	<input type="file" name="myfile2" accept=".pdf" required="required" > </br>
 
     			</div>
     		</div>
     		</br>
     		<div class="row">
-    			<div class="col-md-6 ">
+    			<div class="col-md-4 ">
     				<h2>CV</h2>
     			</div>
-    			<div class="col-md-6">
+    			<div class="col-md-8">
     			  	<input type="file" name="myfile3" accept=".pdf" required="required">  </br>
 
     			</div>
     		</div>
     		<div class="row">
-    			<button type="submit" name="save">upload</button></br>
+    			<button type="submit" style = "margin:auto;BACKGROUND-COLOR: green;color: white;" name="save">UPLOAD</button></br>
 
     		</div>
     	</form>
-    	</div>
+            </div>
+          </div>
+        </div>
+      </div>
+     
     </div>
+  </div>
+   
   </body>
+    <!-- Argon Scripts -->	
+  <!-- Core -->	
+  <script src="assets/vendor/jquery/dist/jquery.min.js"></script>	
+  <script src="assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>	
+  <script src="assets/vendor/js-cookie/js.cookie.js"></script>	
+  <script src="assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>	
+  <script src="assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>	
+  <!-- Argon JS -->	
+  <script src="assets/js/argon.js?v=1.2.0"></script>	
+</body>	
+<script>  	
+ $(document).ready(function(){  	
+      $('#insert').click(function(){  	
+           var image_name = $('#image').val();  	
+           if(image_name == '')  	
+           {  	
+                alert("Please Select Image");  	
+                return false;  	
+           }  	
+           else  	
+           {  	
+                var extension = $('#image').val().split('.').pop().toLowerCase();  	
+                if(jQuery.inArray(extension, ['gif','png','jpg','jpeg']) == -1)  	
+                {  	
+                     alert('Invalid Image File');  	
+                     $('#image').val('');  	
+                     return false;  	
+                }  	
+           }  	
+      });  	
+ });  	
+ </script>  	
 </html>
+
