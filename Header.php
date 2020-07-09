02482f1c7172c2a2f5d4f0acd043ad90333fd68d
sudhanshu1993 <49163737+sudhanshu1993@users.noreply.github.com>
2020-07-09 18:13:40 +0530

    Update Header.php
    <section id="headerBtm"> add

--- Header.php
+++ Header.php
@@ -1,6 +1,7 @@
 
       <link rel="stylesheet" type="text/css" href="css/demo.css">
-<section id="headerBtm" style="box-shadow: 0 5px 8px #d3d3d3 !important;padding: 1%;">
+<!-- <section id="headerBtm" style="box-shadow: 0 5px 8px #d3d3d3 !important;padding: 1%;"> -->
+<section id="headerBtm">
     <div class="container-fluid">
         <nav class="navbar navbar-inverse" role="navigation"> <!-- navbar-fixed-top -->
             <div class="container-fluid-padNone">
