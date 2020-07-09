5bad64f9575d4e3e60175a5984a4a4398a2e9440
soundarya-karli <soundaryakarli@gmail.com>
2020-07-09 13:11:28 +0530

    DOCUMENTS added in admin section for freelancer

---
+++ admin/freelance_docs.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>InnerWork|Admin</title>
+    <?php include 'CommonFiles.php'; ?>
+
+    <link rel="stylesheet" href="css/jobseeker.css">
+    <script src="js/menu.js"></script>
+    <script type="text/javascript" src="js/jobseeker.js"></script>
+
+</head>
+<body>
+<div id="main">
+    <?php include 'Header.php'; ?>
+
+    <section id="enquiries">
+        <div class="container-fluid">
+            <div id="enquiryDataResult"></div>
+            <?php
+            include_once '../DbConnection/DbConnectionHelper.php';
+            if(isset($_GET['id']))
+{
+    $id = $_GET['id'];
+}
+
+
+
+ $stmt = $conn->prepare('select * from freelancer where userid = ?');
+
+ $stmt->bindParam(1, $id);
+            $stmt->execute();
+            if($stmt->rowCount() > 0)
+            {
+                ?>
+                <table class="tbl">
+                    <thead>
+                    <tr>
+                        <th style="width:3%;">Sr. No.</th>
+                        <th style="width:6%;">Adhar</th>
+                        <th style="width:6%;">Pan</th>
+                        <th style="width:6%;">CV</th>
+
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    $cnt = 1;
+                    $data = $stmt->fetchAll();
+                    foreach($data as $row)
+                    {
+                        $adhar_file_raw = $row['adhar'];
+                        $adhar_file = str_replace(" ","%20",$adhar_file_raw); // 12-05-2020
+                        $pan_file_raw = $row['pan'];
+                        $pan_file = str_replace(" ","%20",$pan_file_raw); // 12-05-2020
+                        $cv_file_raw = $row['cv'];
+                        $cv_file = str_replace(" ","%20",$cv_file_raw); // 12-05-2020
+
+                        ?>
+
+                        <tr id="<?php echo $row['userid']; ?>">
+                            <td><?php echo $cnt; ?></td>
+                            <td>
+                                <?php echo "<a href=\"https://innerworkindia.com/freelancerdocuments/aadhar/{$adhar_file}\">"; // 12-05-2020?>
+                                <?php echo $row['adhar']; ?>;
+                                </a>
+                            </td>
+                            <td>
+                                <?php echo "<a href=\"https://innerworkindia.com/freelancerdocuments/PAN/{$pan_file}\">"; // 12-05-2020?>
+                                <?php echo $row['pan']; ?>;
+                                </a>
+                            </td>
+                            <td>
+                                <?php echo "<a href=\"https://innerworkindia.com/freelancerdocuments/CV/{$cv_file}\">"; // 12-05-2020?>
+                                <?php echo $row['cv']; ?>;
+                                </a>
+                            </td>
+
+
+                                                    </tr>
+                                                    <?php
+                                                    $cnt++;
+                                                }
+                                                ?>
+                                                </tbody>
+                                            </table>
+                                            <?php
+                                        }
+                                        else
+                                        {
+                                            echo "<div class='alert alert-info alert-dismissable'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a> <strong>Info!</strong> There is no Enquiries available yet.</div>";
+                                        }
+                                        ?>
+
+                                    </div>
+                                </section>
+
+
+    <!------------------------ -- modals -- ---------------------------->
+
+
+</div>
+</body>
+</html>
---
+++ database/css/Footer.php
@@ -0,0 +1,95 @@
+<section id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-xs-12">
+                <h2><span class="heading">Innerwork</span></h2>
+                <p>Innerwork is emerging as a leader in the next phase of HR Services</p>
+                <a href="about" class="btton">Read More <i class="fa fa-angle-double-right"></i></a>
+                <ul class="contactSocial">
+                    <li><a href="https://www.facebook.com/Innerworkolution/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/innerwork-solution-8a286919b" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="https://instagram.com/innerwork__solutions?igshid=nyewgunv5ra1" target="_blank"><i class="fa fa-instagram"></i></a></li>
+                    <li><a href="https://twitter.com/innverwork " target="_blank"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="https://in.pinterest.com/innerwork123/ " target="_blank"><i class="fa fa-pinterest"></i></a></li>
+                    <li><a href="https://youtu.be/LRL5GkFQTpA" target="_blank"><i class="fa fa-youtube"></i></a></li>
+                </ul>
+            </div>
+            <div class="col-md-4 col-xs-12">
+                <div class="row">
+                    <h3>Quick Links</h3>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <ul class="links">
+                            <li><a href="index">Home</a></li>
+                            <li><a href="about">About Us</a></li>
+                            <li><a href="hr-consultancy">HR Services</a></li>
+                            <li><a href="it-services">IT Services</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6">
+                        <ul class="links">
+                            <li><a href="digital-marketing">Digital Marketing</a></li>
+                            <li><a href="jobseeker">Job Seeker</a></li>
+                            <li><a href="contact">Contact Us</a></li>
+                            <li><a href="termsandconditions">Terms & Conditions</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-xs-12">
+                <div class="contactDetail" style="color:#01131b;">
+                    <h3>Reach Us</h3>
+                    <p><i class="fa fa-map"></i> <span>#80, 4th cross, Aswath Nagar, Marathahalli, Bangalore - 560037</span></p>
+                    <p><i class="fa fa-phone"></i><span>+91 9887888469 / +91 9487980784</span></p>
+                    <p><i class="fa fa-envelope"></i><sapn>Info@innerworkindia.com</sapn></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="footerBottm">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <p>&copy; 2020 Innerwork. All right reserved.</p>
+            </div>
+            <div class="col-md-6">
+                <p class="copyright">
+                    Website Design & Developed By</i> <a href="http://www.innerworkindia.com" target="_blank">Innerwork Solutions</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<script type="text/javascript">
+    (function () {
+        var options = {
+            whatsapp: "+91 9887888469", // WhatsApp number
+            call_to_action: "Message Us", // Call to action
+            position: "left", // Position may be 'right' or 'left'
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+</script>
+
+<!-- Tawk.to -->
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/5e7f141369e9320caabde55f/default';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
