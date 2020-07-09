4b2d69d48d27f5fe8d293b893b825186bb9f3a24
soundarya-karli <soundaryakarli@gmail.com>
2020-07-09 16:24:23 +0530

    freelancer documents changes

--- uplod.php
+++ uplod.php
@@ -7,21 +7,23 @@ include_once 'DbConnection/DbConnectionHelper.php';
 		$filename1 = $_FILES['myfile1']['name'];
     	$tempname1 = $_FILES['myfile1']['tmp_name'];
 
-    	$target_dir1 = "recruiterdocuments/aadhar".$filename1;
+    	$target_dir1 = "recruiterdocuments/aadhar/".$filename1;
 
     	$filename2 = $_FILES['myfile2']['name'];
     	$tempname2 = $_FILES['myfile2']['tmp_name'];
 
-    	$target_dir2 = "recruiterdocuments/PAN".$filename2;
+    	$target_dir2 = "recruiterdocuments/PAN/".$filename2;
 
     	$filename3 = $_FILES['myfile3']['name'];
     	$tempname3 = $_FILES['myfile3']['tmp_name'];
 
-    	$target_dir3 = "recruiterdocuments/CV".$filename3;
+    	$target_dir3 = "recruiterdocuments/CV/".$filename3;
 
     	if(move_uploaded_file($tempname1,$target_dir1)&&move_uploaded_file($tempname2,$target_dir2)&&move_uploaded_file($tempname3,$target_dir3))
     	{ $email=$_SESSION['email'];
-				if(isset($_SESSION['type'])=="Agency"){
+				echo $_SESSION['type'];
+				if(isset($_SESSION['type'])=="Agency")
+				{
 				$sql="UPDATE agency SET adhar=:f1,pan=:f2,cv=:f3  WHERE email=:em";
 				$stmt = $conn->prepare($sql);
         $stmt->execute(array(
@@ -30,9 +32,14 @@ include_once 'DbConnection/DbConnectionHelper.php';
             ':f3' => $filename3,
 						':em' => $email
 						  ));
-
-				}
+		
+						 $no=$stmt->rowCount();
+						 if($no>0){
+							 echo "Updated Successfully!!!!";
+							 header('Location: freelanceraction.php?uploaded=1');}
 				else{
+					$email=$_SESSION['email'];
+
 					$sql="UPDATE freelancer SET adhar=:f1,pan=:f2,cv=:f3  WHERE email=:em";
 					$stmt = $conn->prepare($sql);
 					$stmt->execute(array(
@@ -41,9 +48,12 @@ include_once 'DbConnection/DbConnectionHelper.php';
 							':f3' => $filename3,
 							':em' => $email
 								));
-				}
-    		header('Location: freelanceraction.php?uploaded=1');
+								header('Location: freelanceraction.php?uploaded=1');
+
+			}
+
     	}
+		}
     	else{
     	    header('Location: freelanceraction.php?uploaded=0');
     	}
