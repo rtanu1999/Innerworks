5f9cb55bfe35e023d2851883fef6fbd580529d6a
sudhanshu1993 <49163737+sudhanshu1993@users.noreply.github.com>
2020-07-10 09:39:27 +0530

    Update contact.php
    linkedin link add

--- contact.php
+++ contact.php
@@ -123,7 +123,8 @@ if(isset($_POST['submit']))
             <h4>Follow Us</h4>
             <ul class="socialContact">
                 <li><a href="https://www.facebook.com/InnerworkSolution/" target="_blank"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="https://www.linkedin.com/in/innerwork-solution-8a286919b" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+               
+                <li> <a href="https://www.linkedin.com/company/innerworksolutions/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
                 <li><a href="https://instagram.com/innerwork__solutions?igshid=nyewgunv5ra1" target="_blank"><i class="fa fa-instagram"></i></a></li>
                 <li><a href="https://twitter.com/innverwork " target="_blank"><i class="fa fa-twitter"></i></a></li>
                 <li><a href="https://in.pinterest.com/innerwork123/ " target="_blank"><i class="fa fa-pinterest"></i></a></li>
