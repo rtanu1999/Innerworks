c5d483fa284c9c482a19e2a1c92626af5365caaf
kole-swapnil <swapnilkole7500@gmail.com>
2020-07-16 01:35:01 +0530

    style and icons  added on candidate ajax

--- getcandidateajax.php
+++ getcandidateajax.php
@@ -5,25 +5,31 @@
 
 if(isset($_POST["action"]))
 {
-       $qry1="select * from jobseeker";
-	   $qry2="select * from internship";
-	   $qry = "(select * from jobseeker) UNION (select * from internship)"; 
+       $qry1="select name,email,mobileNum,city,exp,education,skill,file,type from jobseeker";
+	   $qry2="select name,email,mobno,city,exp,education,skill,fnamee,type from internship";
+	   $qry = "(select name,email,mobileNum,city,exp,education,skill,file,type from jobseeker) UNION (select name,email,mobno,city,exp,education,skill,fnamee,type from internship)";
 	  // if(isset($_POST["loc"]) && !empty($_POST["loc"]) )
 		  if(isset($_POST["sectitle"]) && !empty($_POST["sectitle"]))
 	{$name=$_POST['sectitle'];
-		$qry = "(".$qry1." WHERE name like '%$name%' or city like '%$name%' or skill like '%$name%' or exp like '%$name%' or education like '%$name%' ) "."UNION" . " (".$qry2." WHERE name like '%$name%' or city like '%$name%' or skill like '%$name%' or exp like '%$name%' or education like '%$name%' ) ";
-
-
+		$qry = "(".$qry1." WHERE name like '%$name%' or type like '%$name%' or  city like '%$name%' or skill like '%$name%' or exp like '%$name%' or education like '%$name%' ) "."UNION"." (".$qry2." WHERE name like '%$name%' or type like '%$name%' or  city like '%$name%' or skill like '%$name%' or exp like '%$name%' or education like '%$name%' ) ";
 
 
 	}
+  if(isset($_POST["type"]))
+{
+$type_filter = implode("','", $_POST["type"]);
+
+ $qry = "(".$qry1." WHERE type IN('".$type_filter."')) "."UNION" . " (".$qry2." WHERE type IN('".$type_filter."'))";
+
+}
 		   if(isset($_POST["loc"]))
 	{
 		$loc_filter = implode("','", $_POST["loc"]);
+
 			$qry = "(".$qry1." WHERE city IN('".$loc_filter."')) "."UNION" . " (".$qry2." WHERE city IN('".$loc_filter."'))";
-		
-    $skills_filter = implode("','", $_POST["skills"]);
-    $qry = "(".$qry1." WHERE skill IN('".$skills_filter."')) "."UNION" . " (".$qry2." WHERE skill IN('".$skills_filter."'))";
+
+
+
 
 	}
 
@@ -72,10 +78,10 @@ if(isset($_POST["edu"]))
 			<div class="job-card-description">
 			<div class="internships-tabs">
 
-			<div class="nav nav-pills" style="width:100%;height:100%;background-color:#ffc114;padding-left: 1%">
+			<div class="nav nav-pills" style="width:100%;height:100%;background-color:#ffc114;padding-left: 1%;">
 			<div class="job-title">
 
-                <b><h4 class="truncate-normal" title="'. $row['name'] .'" data-etracking="true" >'. $row['name'] .'</h4>
+                <b><h4 class="truncate-normal" title="'. $row['name'] .'" data-etracking="true" >'. $row['name'] .'-'. $row['type'] .'</h4>
                 </b>
                 </div></div>
                 <div class="company-name"><p class="truncate-normal" style = "padding-left: 1%;margin-bottom:-15px;" title="' .$row['mobileNum'] .'" data-etracking="true">' .$row['mobileNum'] .'</p>
@@ -89,16 +95,17 @@ if(isset($_POST["edu"]))
                 <div class="row job-info no-margin-left no-margin-right">
 
         <ul class="list-group" style="paddding-left:6% !important;">
-          <li class="list-group-item" title="' .$row['exp'] .'" data-etracking="true">Experience: ' .$row['exp'] .'
+        
+          <li class="list-group-item" title="Experience" data-etracking="true"><i class="fa fa-hourglass-2"></i>' .$row['exp'] .'
            </li>
-          <li class="list-group-item" title="Compensation" data-etracking="true" data-ecategory="job_card_compensation">
-        Skills: ' .$row['skill'] .'
+          <li class="list-group-item" title="Skills" data-etracking="true" data-ecategory="job_card_skill">
+        <i class="fa fa-user" ></i> ' .$row['skill'] .'
           </li>
           <li class="list-group-item" title="Qualification" data-etracking="true" data-ecategory="job_card_start_date">
-          Education: ' .$row['education'] .'
+          <i class="fa fa-book" ></i>' .$row['education'] .'
           </li>
           <li class="list-group-item" title="Resume" data-etracking="true" data-ecategory="job_card_start_date">
-          View Resume:<a href=\"https://innerworkindia.com/Resume/{$Resume_file}\"> ' .$row['file'] .'</a>
+          <i class="fa fa-file-word-o" ></i><a href=\"https://innerworkindia.com/Resume/{$Resume_file}\"> ' .$row['file'] .'</a>
           </li>
           </ul></div>
 
@@ -117,7 +124,7 @@ if(isset($_POST["edu"]))
 	}
 	else
 	{
-		$output = '<h4>No Job Found</h4>';
+		$output = '<h2>No Candidates Found!</h2>';
 	}
 	//echo '<pre>'; print_r($_POST["loc"]); echo '</pre>';
 	echo $output;
