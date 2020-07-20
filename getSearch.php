1ad86d56b987dfc04728fe56bcaab711216c9c95
rahulbandodkar <59685264+rahull04@users.noreply.github.com>
2020-07-20 13:16:25 +0530

    a

--- getSearch.php
+++ getSearch.php
@@ -3,7 +3,7 @@ include "DbConnection/DbConnectionHelper.php";
 
 //get the q parameter from URL
  $q=$_GET["q"];
-
+$val=$_GET["val"];
 
 	
    $stmt = $conn->prepare("select * from tbl_skills where skill_name like '%".$q."%' order by skill_name asc limit 5");
@@ -15,18 +15,34 @@ include "DbConnection/DbConnectionHelper.php";
 
         $data = $stmt->fetchAll();
 		 $search_arr = array();
-		 
-        foreach($data as $row) {
+		 if($val==1){
+             
+                foreach($data as $row) {
+
+                $name = $row['skill_name'];
+                 //$search_arr[] = array($name);
+                 ?>
+                 <p onclick='fill("<?php echo $name; ?>")'><a> 
+           <!-- Assigning searched result in "Search box" in "search.php" file. -->
+               <?php echo $name; ?>
+           </a></p>
+
+            <?php    }
+         }
+        if($val==2){
+            
+                    foreach($data as $row) {
        
         $name = $row['skill_name'];
 		 //$search_arr[] = array($name);
 		 ?>
-		 <p onclick='fill("<?php echo $name; ?>")'><a> 
+		 <p onclick='fillinterst("<?php echo $name; ?>")'><a> 
    <!-- Assigning searched result in "Search box" in "search.php" file. -->
        <?php echo $name; ?>
    </a></p>
 		
     <?php    }
+        }
 
         }
 	  //echo json_encode($search_arr);
